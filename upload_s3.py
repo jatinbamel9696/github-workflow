3176559e9d0800d9ff403c1b889816eece82a4be
jatinbamel9696 <45420350+jatinbamel9696@users.noreply.github.com>
2024-11-25 12:01:10 +0530

    test

--- upload_s3.py
+++ upload_s3.py
@@ -8,16 +8,14 @@ from botocore.exceptions import BotoCoreError, ClientError
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger()
 
-def upload_to_s3(bucket_name, local_report_path, github_run_id, include_extensions=None, include_files=None):
+def upload_to_s3(bucket_name, local_report_path, github_run_id):
     """
-    Uploads specific reports to the specified S3 bucket with the folder structure Reports/Year/Month/Day/github_run_id/reports.
+    Uploads all files from the local reports directory to the specified S3 bucket.
 
     Parameters:
         bucket_name (str): Name of the S3 bucket.
         local_report_path (str): Path to the local reports directory.
         github_run_id (str): GitHub Run ID.
-        include_extensions (list): List of file extensions to include (e.g., ['.txt', '.json']).
-        include_files (list): List of specific file names to include (e.g., ['report1.txt', 'summary.json']).
     """
     try:
         # Assume the AWS role using environment-provided credentials
@@ -33,15 +31,9 @@ def upload_to_s3(bucket_name, local_report_path, github_run_id, include_extensio
             logger.error(f"Local path '{local_report_path}' does not exist.")
             return
 
-        # Upload specific files from the local reports directory to S3
+        # Upload all files from the local reports directory to S3
         for root, _, files in os.walk(local_report_path):
             for file in files:
-                # Check if the file matches the criteria for upload
-                if include_extensions and not file.endswith(tuple(include_extensions)):
-                    continue
-                if include_files and file not in include_files:
-                    continue
-
                 local_file_path = os.path.join(root, file)
                 s3_key = s3_prefix + file
 
@@ -62,10 +54,6 @@ if __name__ == "__main__":
     LOCAL_REPORT_PATH = os.getenv("LOCAL_REPORT_PATH")  # Example: './reports'
     GITHUB_RUN_ID = os.getenv("GITHUB_RUN_ID")         # GitHub Run ID passed as an environment variable
 
-    # Define the criteria for files to upload
-    INCLUDE_EXTENSIONS = [".log", ".html"]  # Upload only .log and .html files
-    INCLUDE_FILES = ['aws.json']  # Example: ['specific-report.txt', 'summary.json'], or set to None to ignore
-
     # Validate mandatory inputs
     if not BUCKET_NAME:
         logger.error("S3_BUCKET_NAME is required but not set.")
@@ -74,4 +62,4 @@ if __name__ == "__main__":
     elif not GITHUB_RUN_ID:
         logger.error("GITHUB_RUN_ID is required but not set.")
     else:
-        upload_to_s3(BUCKET_NAME, LOCAL_REPORT_PATH, GITHUB_RUN_ID, INCLUDE_EXTENSIONS, INCLUDE_FILES)
+        upload_to_s3(BUCKET_NAME, LOCAL_REPORT_PATH, GITHUB_RUN_ID)
